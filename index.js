e761f14a17eb924c59b47a67d3eb4e0ab364ca63
Derrick korir kiptoo <derrickroz4227@gmail.com>
2024-04-17 01:54:44 +0300

    initail

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+// Function to move the dodger left
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  // Function to move the dodger right
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+  
+    const gameFieldWidth = window.innerWidth;
+    const dodgerWidth = dodger.offsetWidth;
+  
+    if (left < gameFieldWidth - dodgerWidth) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
